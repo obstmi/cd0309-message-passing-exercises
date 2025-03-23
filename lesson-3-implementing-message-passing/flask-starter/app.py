227ade6f3156bd26cf3433a18f5ac59391101d8c
Michael Obst <michael.obst@telekom.de>
2025-03-23 21:14:35 +0100

    exercise-4-7-a

--- lesson-3-implementing-message-passing/flask-starter/app.py
+++ lesson-3-implementing-message-passing/flask-starter/app.py
@@ -1,14 +1,21 @@
 import json
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 
 from .services import retrieve_orders, create_order
 
 app = Flask(__name__)
 
 
-@app.route('/health')
-def health():
-    return jsonify({'response': 'Hello World!'})
+@app.route('/orders', methods=['GET', 'POST'])
+def orders():
+    if request.method == 'GET':
+        return Response(json.dumps(retrieve_orders()), 200, {'Content-Type': 'application/json'})
+    elif request.method == 'POST':
+        order = request.json
+        return jsonify(create_order(order))
+    else:
+        raise Exception('Unsupported HTTP request type.')
+    
 
 if __name__ == '__main__':
     app.run()
