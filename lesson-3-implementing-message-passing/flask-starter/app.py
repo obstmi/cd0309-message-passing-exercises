93505d241b34540cc7e1634c5131ea57e8ca2034
Michael Obst <michael.obst@telekom.de>
2025-03-23 22:39:07 +0100

    exercise-4-7-b

--- lesson-3-implementing-message-passing/flask-starter/app.py
+++ lesson-3-implementing-message-passing/flask-starter/app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, jsonify, request, Response
+from flask import Flask, jsonify, request, Response, make_response
 
 from .services import retrieve_orders, create_order
 
@@ -11,8 +11,18 @@ def orders():
     if request.method == 'GET':
         return Response(json.dumps(retrieve_orders()), 200, {'Content-Type': 'application/json'})
     elif request.method == 'POST':
-        order = request.json
-        return jsonify(create_order(order))
+        request_body = request.json
+
+        # JSONify response
+        response = make_response(jsonify(create_order(request_body)), 201)
+
+        # alternatively, but without status code:
+        # response = jsonify(create_order(request_body))
+
+        # Add Access-Control-Allow-Origin header to allow cross-site request
+        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+
+        return response
     else:
         raise Exception('Unsupported HTTP request type.')
     
