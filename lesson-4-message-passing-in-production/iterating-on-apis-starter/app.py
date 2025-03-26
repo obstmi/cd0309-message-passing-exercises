f3b7d40a2c6161f07587059ea91480a202c71f3a
Michael Obst <michael.obst@telekom.de>
2025-03-26 15:09:01 +0100

    exercise-5-15

--- lesson-4-message-passing-in-production/iterating-on-apis-starter/app.py
+++ lesson-4-message-passing-in-production/iterating-on-apis-starter/app.py
@@ -21,6 +21,18 @@ def computers():
     else:
         raise Exception('Unsupported HTTP request type.')
 
+# or - to differentiate the version of the API:
+# @app.route('/api/v2/orders/computers', methods=['GET', 'POST'], endpoint='orders_v2')
+@app.route('/api/v2/orders/computers', methods=['GET', 'POST'])
+def computers_v2():
+    if request.method == 'GET':
+        return jsonify(retrieve_orders())
+    elif request.method == 'POST':
+        request_body = request.json
+        return jsonify(create_order(request_body))
+    else:
+        raise Exception('Unsupported HTTP request type.')
+
 
 if __name__ == '__main__':
     app.run()
